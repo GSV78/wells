9ec9565334cad7b6b868faa8a00e34af329c2c0b
GSV78 <gradov78@gmail.com>
2022-02-28 19:03:38 +0300

    Deploy on heroku4

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,16 @@
 import * as axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:3001',
-});
-
 export const getLibFromServer = () => {
-  return instance.get('/lib').then((responce) => responce.data);
+  return axios.get(`lib`).then((responce) => responce.data);
 };
 export const addNewItem = (values) => {
-  return instance.post('/lib', values).then((responce) => responce.status);
+  return axios.post('/lib', values).then((responce) => responce.status);
 };
 export const deleteItemFromServer = (id) => {
-  return instance.delete(`/lib/${id}`).then((responce) => responce.status);
+  return axios.delete(`/lib/${id}`).then((responce) => responce.status);
 };
 export const putNewPriceToServer = (id, name, newPrice, unit, category) => {
-  return instance
+  return axios
     .put(`/lib/${id}`, { name, price: newPrice, unit, category })
     .then((responce) => responce.status);
 };
